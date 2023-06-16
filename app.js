9c94ddf62760c3ed8a377d522f63c822d58fafa7
joshnyaga <joshkish55@gmail.com>
2023-06-16 04:24:38 +0300

    Setting up app.js

---
+++ app.js
@@ -0,0 +1,47 @@
+// imports
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// database connection
+const connect = () => {
+    mongoose
+      .connect(process.env.MONGO)
+      .then(() => {
+        console.log("connected to db");
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
+
+//middlewares
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(session({
+    secret: "my secret key",
+    saveUninitialized:true,
+    resave:false
+}))
+app.use((req,res, next)=>{
+    res.local.message = req.session.message;
+    delete req.session.message;
+    next();
+})
+
+// set template engine
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server started at http://localhost:${PORT}`);
+})
