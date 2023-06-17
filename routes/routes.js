08418d54e38938fabe90db22fd46841a4c390095
joshnyaga <joshkish55@gmail.com>
2023-06-17 14:48:56 +0300

    Adding bootstrap instead

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const title = "Kongoni Community Library";
 
 router.get("/", (req,res)=>{
-    res.render("index", {title: `${title} - Homepage`})
+    res.render("index", {title: `${title} - Homepage`});
+   
+})
+router.get("/login", (req,res)=>{
+    res.render("login", {title: `${title} - Login   `});
 })
 
 module.exports = router;
